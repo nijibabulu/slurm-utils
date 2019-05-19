55d9e975b27ed8f333324074ffa25d51b84036f7
Bob Zimmermann <robert.zimmermann@univie.ac.at>
2019-05-19 18:21:32 +0200

    switch from parsec to regex-applicative

--- tmp_rewrite/Main.hs
+++ tmp_rewrite/Main.hs
@@ -14,24 +14,26 @@ import qualified Options.Applicative.Help.Pretty as P
 import           System.Directory
 import           System.FilePath.Posix
 import           System.IO ( stdin )
-import           Text.ParserCombinators.Parsec
+import           Text.Regex.Applicative
 import           Text.Read ( readEither )
 
-data CmdPart = RestChar Char        -- a non-translated character of the command
-             | Rest String          -- a non-translated string of the command
-             | InputFile String     -- an input file
-             | OutputFile String    -- an output file
-             | OutputDir String     -- an output directory
-             deriving Show
+data CmdPart =   Rest String          -- a non-translated string of the command
+               | InputFile String     -- an input file
+               | OutputFile String    -- an output file
+               | OutputDir String     -- an output directory
+               | InvalidRewriteable String
+               deriving Show
 
 rest :: CmdPart -> String
 rest (Rest s) = s
 
+-- helper function to filter command parts of certain types
+applyFileName :: [CmdPart -> Maybe String] -> CmdPart -> Maybe String
 applyFileName fs x =
     let filenames = catMaybes $ map (\f -> f x) fs
     in case filenames of 
         [] -> Nothing
-        [f] -> Just f
+        [fn] -> Just fn
 
 -- accessors for input and output file names
 inputFileName, outputFileName, outputDirName, rewriteableFileName, outputPath :: 
@@ -42,49 +44,33 @@ outputDirName x = case x of {(OutputDir s) -> Just s; otherwise -> Nothing}
 rewriteableFileName =  applyFileName [inputFileName, outputFileName, outputDirName]
 outputPath = applyFileName [outputFileName, outputDirName]
 
--- flatten many RestChars in to a single Rest
-flattenRestChars :: [CmdPart] -> CmdPart
-flattenRestChars xs@((RestChar h):t) = Rest $ map (\(RestChar c) -> c) xs
-flattenRestChars xs@(h:[]) = h
+bracedToken :: RE Char [Char] -> RE Char [Char]
+bracedToken f =  sym '{' *> many (psym (/= ':')) <* sym ':' <* f <* sym '}'
 
--- Determine if two CmdParts are both RestChars
-matchRestChar :: CmdPart -> CmdPart -> Bool
-matchRestChar (RestChar _) (RestChar _) = True
-matchRestChar _ _ = False
+rewriteableToken :: [Char] -> RE Char [Char]
+rewriteableToken c =  bracedToken $ string c 
 
 -- Parse the command into a list of command parts
+cmdToken :: RE Char CmdPart
+cmdToken = (InputFile <$> rewriteableToken "i") 
+    <|> (OutputFile <$> rewriteableToken "o")
+    <|> (OutputDir <$> rewriteableToken "d")
+    <|> (InvalidRewriteable <$> bracedToken (many anySym))
+
 parseCmd :: String -> Either String [CmdPart]
-parseCmd str =
-  let parser = many tmpCmdParser
-    in case parse parser "" str of
-        Left err -> Left $ show err
-        Right parts ->
-          Right $ map flattenRestChars $ groupBy matchRestChar parts
+parseCmd s = let parseCmd' cs r s@(h:t) = 
+                  let next = findFirstPrefix cmdToken s in
+                        case next of
+                          Just (InvalidRewriteable e, _) -> Left $ "Invalid rewriteable:" ++ e ++ "\n" ++ rewriteableInfo
+                          Just (cmd, suf) -> case r of 
+                                      "" -> parseCmd' (cs ++ [cmd]) "" suf
+                                      r -> parseCmd' (cs ++ [Rest r] ++ [cmd]) "" suf
+                          Nothing -> parseCmd' cs (r ++ [h]) t
+                 parseCmd' cs "" "" = Right cs
+                 parseCmd' cs r "" = Right (cs ++ [Rest r])
+              in parseCmd' [] "" s
 
-tmpCmdParser :: Parser CmdPart 
-tmpCmdParser =
-  try inputParser <|> try outputParser <|> try dirParser <|>
-    restParser <?> "Parse error"
-
-restParser :: Parser CmdPart
-restParser = RestChar <$> anyChar
-
--- the parser generator
--- t is the type of the output
--- c is the character expected in the {PATH:c} syntax
-gen_parser :: (String -> CmdPart) -> Char -> Parser CmdPart
-gen_parser t c = do
-    t <$> between (string "{")
-                  (string $ ":" ++ [c] ++ "}")
-                  (many $ satisfy (\a -> a/= ':'))
-
-inputParser, outputParser, dirParser :: Parser CmdPart
-inputParser = gen_parser InputFile 'i'
-outputParser = gen_parser OutputFile 'o'
-dirParser = gen_parser OutputDir 'd'
-
-
-
+choicesReader :: [String] -> O.ReadM String
 choicesReader xs = O.eitherReader $ checkInput xs
   where
         checkInput xs input 
@@ -124,7 +110,7 @@ cpRewrite s cmdParts =
    in cpRewrite' inputs (mode s)
 
 cmdRewrite :: TmpSettings -> [CmdPart] -> Maybe String
-cmdRewrite s cmdParts = Just $ foldl (++) "" $ map rewritePart $ cmdParts
+cmdRewrite s cmdParts = Just $ foldl (++) "" $ map rewritePart cmdParts
   where
     rewritePart part 
          | Just p <- rewriteableFileName part = (rewriteToTmp s p)
@@ -195,34 +181,37 @@ tmpOptions =
   TmpSettings <$>
   O.option
     (choicesReader modes)
-    (O.long "mode" <> O.value "lock" <> O.showDefault <>
-     O.helpDoc (Just modeHelpDoc)) <*>
+    (O.long "mode" <> O.value "lock" <> O.showDefault 
+  <> O.helpDoc (Just modeHelpDoc)) <*>
   O.option
     O.auto
-    (O.long "wait" <> O.value 7200 <> O.showDefault <>
-     O.help "How long to wait before giving up the lock") <*>
+    (O.long "wait" <> O.value 7200 <> O.showDefault 
+  <> O.help "How long to wait before giving up the lock") <*>
   O.strOption
-    (O.long "tmploc" <> O.value "$TMPDIR" <> O.showDefault <>
-     O.help "Location of the temporary directory to copy to") <*>
+    (O.long "tmploc" <> O.value "$TMPDIR" <> O.showDefault 
+  <> O.help "Location of the temporary directory to copy to") <*>
   O.strOption
-    (O.long "cpcmd" <> O.value "cp -Lr" <> O.showDefault <>
-     O.help "Copy command to use") <*>
+    (O.long "cpcmd" <> O.value "cp -Lr" <> O.showDefault 
+  <> O.help "Copy command to use") <*>
   O.strOption
-    (O.long "lockfile" <> O.value "LOCK" <> O.showDefault <>
-     O.help "Name of the lock file") <*>
+    (O.long "lockfile" <> O.value "LOCK" <> O.showDefault 
+  <> O.help "Name of the lock file") <*>
   O.switch 
-    (O.long "ignore-errors" <> 
-     O.help "Do not check for missing files before rewriting") <*>
+    (O.long "ignore-errors"  
+  <> O.help "Do not check for missing files before rewriting") <*>
   O.many (O.argument O.str (O.metavar "TEMPLATE..."))
 
+rewriteableInfo :: String
+rewriteableInfo = "Rewriteables use a formatting syntax of {INPUT:i} for input " 
+              ++  "file INPUT, {DIR:d} for output directory DIR (DIR is created) " 
+              ++  "and {OUTPUT:o} for output file OUTPUT."
+
 progDescDoc = P.vsep $
-  [ paragraph ("Automatically wrap a command with copies to and" ++
-               "from a temporary directory and rewrite the " ++
-               "command to use files in the temporary directory. " ++
-               "Uses a formatting syntax of {INPUT:i} for input " ++
-               "file INPUT, {DIR:d} for output directory DIR (DIR " ++ 
-               "is created) and {OUTPUT:o} for output file OUTPUT." ++
-               "For example,")
+  [ paragraph ("Automatically wrap a command with copies to and" 
+            ++ "from a temporary directory and rewrite the " 
+            ++ "command to use files in the temporary directory. "
+            ++ rewriteableInfo
+            ++ "For example,")
   , P.text ""
   , P.text  "tmp_rewrite \"cat {infile1:i} {infile2:i} > {outfile:o}\""
   , P.text ""
@@ -263,4 +252,3 @@ main = do
 
   out <- mapM (rewrite settings) ls
   putStrLn $ intercalate "\n" out
-
